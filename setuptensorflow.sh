03a3d3a8eeef4513c07a7e0e4e563727b2494c3c
vakinwande <viktour19@gmail.com>
2017-05-30 22:42:42 +0200

    2

---
+++ setuptensorflow.sh
@@ -0,0 +1,23 @@
+alias python=python3
+
+sudo apt-get install pip
+
+sudo apt-get install python3-pip
+
+pip3 install virtualenv
+
+mkdir `pwd`/tflow
+
+virtualenv `pwd`/tflow
+
+source /tflow/bin/activate
+
+pip3 install tensorflow
+
+sudo apt-get nstall ipython ipython-notebook
+
+pip3 install numpy scipy matplotlib pandas Pillow jupyter
+
+jupyter notebook --generate-config
+
+wget https://raw.githubusercontent.com/Viktour19/scripts_configs/master/jupyter_notebook_config.py
